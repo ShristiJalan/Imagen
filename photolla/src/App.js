9e6e6608885e92ae74aab5c12ee8736a4b89fb94
ShristiJalan <shristijalan1010@gmail.com>
2021-03-29 23:21:54 +0530

    Upload form added

--- photolla/src/App.js
+++ photolla/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './comps/Title';
+import UploadForm from './comps/UploadForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm />
     </div>
   );
 }
